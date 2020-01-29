78bbd00cb3d31d8003f6e2a368b152058abe96d1
Komar0015 <kamil.kolodziejski15@gmail.com>
2020-01-29 22:01:16 +0100

    initial commit

---
+++ Assets/Scripts/CanvasMenager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasMenager : MonoBehaviour
+{
+    public GameObject CloseButton;
+    public GameObject ExampleText;
+
+    public void ClickPopupButton()
+    {
+        CloseButton.SetActive(true);
+    }
+
+    public void ClickTimeButton()
+    {
+        ExampleText.SetActive(true);
+        StartCoroutine(ShowExampleText(3.0f));
+    }
+
+    public void ClickCloseButton()
+    {
+        CloseButton.SetActive(false);
+    }
+
+    IEnumerator ShowExampleText(float WaitTime)
+    {
+        yield return new WaitForSeconds(WaitTime);
+        ExampleText.SetActive(false);
+    }
+}
